25788ea111728cb44c1f4717d1a81c361d4734f3
yuki ohno <jeanadler33@gmail.com>
2021-06-07 13:13:35 +0000

    追加: フォロー機能モデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_115914) do
+ActiveRecord::Schema.define(version: 2021_06_07_131226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,16 @@ ActiveRecord::Schema.define(version: 2021_06_07_115914) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["guide_id"], name: "index_pictures_on_guide_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
