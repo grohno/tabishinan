0ac09e95ac81374deb38ed0df1479798d29b79f1
yuki ohno <jeanadler33@gmail.com>
2021-06-07 13:15:40 +0000

    追加: フォロー機能のコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'relationships/index'
+  get 'relationships/create'
+  get 'relationships/destroy'
   resources :guides
   devise_for :users, controllers: {
     registrations: 'users/registrations',
