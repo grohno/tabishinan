6fb7b9072e8bdaa44fc77b549bb21c1b874fcfdc
yuki ohno <jeanadler33@gmail.com>
2021-06-07 12:03:47 +0000

    追加: お気に入り機能のコントローラーに各アクションを設定

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,18 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @favorites = current_user.favorites.all
+  end
+
+  def create
+    favorite = current_user.favorites.create(guide_id: params[:guide_id])
+    redirect_to guide_path(params[:guide_id]), notice: I18n.t('views.messages.register_favorite')
+  end
+
+  def destroy
+    guide_id = current_user.favorites.find_by(id: params[:id]).guide_id
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to guide_path(guide_id), notice: I18n.t('views.messages.delete_favorite')
+  end
 end
