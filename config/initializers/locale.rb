6d2492eaed81ee8de10e91bc663cda4c3648ec8b
yuki ohno <jeanadler33@gmail.com>
2021-06-07 14:19:25 +0000

    追加: シードデータを反映

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,2 +1,9 @@
-I18n.config.available_locales = :ja
-I18n.default_locale = :ja
+require 'i18n'
+I18n.locale # => :en
+I18n.locale = :ja
+I18n.locale # => :ja
+require 'faker'
+Faker::Config.locale # => :ja
+Faker::Config.locale = :en
+Faker::Config.locale # => :en
+Faker::Internet.email # => "ransom_blanda@auer.name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,186 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Faker::Config.locale = :ja
+
+name = "yukiyukiyuki"
+email = "yuki@ohno.com"
+password = "yukiyuki"
+self_introduction = "# ここに自己紹介文が入ります\r\n\r\n## 見出しテスト\r\n\r\n### 見出しテスト\r\n\r\n> 引用テスト\r\n> 引用テスト\r\n> 引用テスト\r\n\r\n- リスト\r\n- リスト\r\n- リスト\r\n\r\n`hogehogefugafuga`\r\n\r\n```\r\nhello world!\r\nhello Ruby on Rails!\r\n```\r\n\r\nhttps://www.google.com/"
+User.create!(name: name,
+             email: email,
+             password: password,
+             password_confirmation: password,
+             self_introduction: self_introduction,
+             image: open("#{Rails.root}/db/user_images/user_image_1.jpg"),
+             admin: true,
+             )
+
+name = "kazukazukazu"
+email = "kazu@ohno.com"
+password = "kazukazu"
+self_introduction = "# ここに自己紹介文が入ります\r\n\r\n## 見出しテスト\r\n\r\n### 見出しテスト\r\n\r\n> 引用テスト\r\n> 引用テスト\r\n> 引用テスト\r\n\r\n- リスト\r\n- リスト\r\n- リスト\r\n\r\n`hogehogefugafuga`\r\n\r\n```\r\nhello world!\r\nhello Ruby on Rails!\r\n```\r\n\r\nhttps://www.google.com/"
+User.create!(name: name,
+            email: email,
+            password: password,
+            password_confirmation: password,
+            self_introduction: self_introduction,
+            image: open("#{Rails.root}/db/user_images/user_image_2.jpg"),
+            admin: false
+            )
+
+name = "hisahisahisa"
+email = "hisa@ohno.com"
+password = "hisahisa"
+self_introduction = "# ここに自己紹介文が入ります\r\n\r\n## 見出しテスト\r\n\r\n### 見出しテスト\r\n\r\n> 引用テスト\r\n> 引用テスト\r\n> 引用テスト\r\n\r\n- リスト\r\n- リスト\r\n- リスト\r\n\r\n`hogehogefugafuga`\r\n\r\n```\r\nhello world!\r\nhello Ruby on Rails!\r\n```\r\n\r\nhttps://www.google.com/"
+User.create!(name: name,
+            email: email,
+            password: password,
+            password_confirmation: password,
+            self_introduction: self_introduction,
+            image: open("#{Rails.root}/db/user_images/user_image_3.jpg"),
+            admin: false
+            )
+
+name = "masamasamasa"
+email = "masa@ohno.com"
+password = "masamasa"
+self_introduction = "# ここに自己紹介文が入ります\r\n\r\n## 見出しテスト\r\n\r\n### 見出しテスト\r\n\r\n> 引用テスト\r\n> 引用テスト\r\n> 引用テスト\r\n\r\n- リスト\r\n- リスト\r\n- リスト\r\n\r\n`hogehogefugafuga`\r\n\r\n```\r\nhello world!\r\nhello Ruby on Rails!\r\n```\r\n\r\nhttps://www.google.com/"
+User.create!(name: name,
+            email: email,
+            password: password,
+            password_confirmation: password,
+            self_introduction: self_introduction,
+            image: open("#{Rails.root}/db/user_images/user_image_4.jpg"),
+            admin: false
+            )
+
+16.times do |n|
+  name = Faker::Games::Pokemon.name
+  email = Faker::Internet.email
+  password = "password"
+  self_introduction = "# ここに自己紹介文が入ります\r\n\r\n## 見出しテスト\r\n\r\n### 見出しテスト\r\n\r\n> 引用テスト\r\n> 引用テスト\r\n> 引用テスト\r\n\r\n- リスト\r\n- リスト\r\n- リスト\r\n\r\n`hogehogefugafuga`\r\n\r\n```\r\nhello world!\r\nhello Ruby on Rails!\r\n```\r\n\r\nhttps://www.google.com/"
+  image_num = n + 5
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               self_introduction: self_introduction,
+               image: open("#{Rails.root}/db/user_images/user_image_#{image_num}.jpg"),
+               admin: false
+               )
+end
+
+6.times do |n|
+  title = Faker::Games::Pokemon.move
+  content = "# ここに本文テキストが入ります。\r\n\r\n## 見出しテスト\r\n\r\n### 見出しテスト\r\n\r\n> 引用テスト\r\n> 引用テスト\r\n> 引用テスト\r\n\r\n- リスト\r\n- リスト\r\n- リスト\r\n\r\n`hogehogefugafuga`\r\n\r\n```\r\nhello world!\r\nhello Ruby on Rails!\r\n```\r\n\r\nhttps://www.google.com/"
+  prefecture = n + 1
+  address = Faker::Address.full_address
+  note = "# ここに備考テキストが入ります。\r\n\r\n## 見出しテスト\r\n\r\n### 見出しテスト\r\n\r\n> 引用テスト\r\n> 引用テスト\r\n> 引用テスト\r\n\r\n- リスト\r\n- リスト\r\n- リスト\r\n\r\n`hogehogefugafuga`\r\n\r\n```\r\nhello world!\r\nhello Ruby on Rails!\r\n```\r\n\r\nhttps://www.google.com/"
+  user_id = 1
+  Guide.create!(title: title,
+               content: content,
+               prefecture: prefecture,
+               address: address,
+               note: note,
+               user_id: user_id,
+               )
+end
+
+6.times do |n|
+  title = Faker::Games::Pokemon.move
+  content = "# ここに本文テキストが入ります。\r\n\r\n## 見出しテスト\r\n\r\n### 見出しテスト\r\n\r\n> 引用テスト\r\n> 引用テスト\r\n> 引用テスト\r\n\r\n- リスト\r\n- リスト\r\n- リスト\r\n\r\n`hogehogefugafuga`\r\n\r\n```\r\nhello world!\r\nhello Ruby on Rails!\r\n```\r\n\r\nhttps://www.google.com/"
+  prefecture = n + 7
+  address = Faker::Address.full_address
+  note = "# ここに備考テキストが入ります。\r\n\r\n## 見出しテスト\r\n\r\n### 見出しテスト\r\n\r\n> 引用テスト\r\n> 引用テスト\r\n> 引用テスト\r\n\r\n- リスト\r\n- リスト\r\n- リスト\r\n\r\n`hogehogefugafuga`\r\n\r\n```\r\nhello world!\r\nhello Ruby on Rails!\r\n```\r\n\r\nhttps://www.google.com/"
+  user_id = 2
+  Guide.create!(title: title,
+               content: content,
+               prefecture: prefecture,
+               address: address,
+               note: note,
+               user_id: user_id,
+               )
+end
+
+14.times do |n|
+  title = Faker::Games::Pokemon.move
+  content = "# ここに本文テキストが入ります。\r\n\r\n## 見出しテスト\r\n\r\n### 見出しテスト\r\n\r\n> 引用テスト\r\n> 引用テスト\r\n> 引用テスト\r\n\r\n- リスト\r\n- リスト\r\n- リスト\r\n\r\n`hogehogefugafuga`\r\n\r\n```\r\nhello world!\r\nhello Ruby on Rails!\r\n```\r\n\r\nhttps://www.google.com/"
+  prefecture = n + 13
+  address = Faker::Address.full_address
+  note = "# ここに備考テキストが入ります。\r\n\r\n## 見出しテスト\r\n\r\n### 見出しテスト\r\n\r\n> 引用テスト\r\n> 引用テスト\r\n> 引用テスト\r\n\r\n- リスト\r\n- リスト\r\n- リスト\r\n\r\n`hogehogefugafuga`\r\n\r\n```\r\nhello world!\r\nhello Ruby on Rails!\r\n```\r\n\r\nhttps://www.google.com/"
+  user_id = n + 3
+  Guide.create!(title: title,
+               content: content,
+               prefecture: prefecture,
+               address: address,
+               note: note,
+               user_id: user_id,
+               )
+end
+
+1.upto(26) do |n|
+  guide_id = n
+  image_num = n
+  Picture.create!(guide_id: guide_id,
+               image: open("#{Rails.root}/db/main_flags/main_flag_image_#{image_num}.jpg"),
+               main_flag: true
+              )
+end
+
+1.upto(26) do |n|
+  guide_id = n
+  image_num = n
+  Picture.create!(guide_id: guide_id,
+               image: open("#{Rails.root}/db/pictures/test_image_#{image_num}.jpg"),
+               main_flag: false
+              )
+end
+
+1.upto(26) do |n|
+  guide_id = n
+  image_num = n + 25
+  Picture.create!(guide_id: guide_id,
+               image: open("#{Rails.root}/db/pictures/test_image_#{image_num}.jpg"),
+               main_flag: false
+              )
+end
+
+1.upto(26) do |n|
+  guide_id = n
+  image_num = n + 7
+  Picture.create!(guide_id: guide_id,
+               image: open("#{Rails.root}/db/pictures/test_image_#{image_num}.jpg"),
+               main_flag: false
+              )
+end
+
+1.upto(26) do |n|
+  guide_id = n
+  image_num = n + 17
+  Picture.create!(guide_id: guide_id,
+               image: open("#{Rails.root}/db/pictures/test_image_#{image_num}.jpg"),
+               main_flag: false
+              )
+end
+
+6.times do |n|
+  user_id = 1
+  guide_id = n + 2
+  Favorite.create!(user_id: user_id,
+               guide_id: guide_id,
+               )
+end
+
+6.times do |n|
+  user_id = 2
+  guide_id = n + 10
+  Favorite.create!(user_id: user_id,
+               guide_id: guide_id,
+               )
+end
+
+13.times do |n|
+  follower_id = 1
+  followed_id = n + 2
+  Relationship.create!(follower_id: follower_id,
+               followed_id: followed_id,
+               )
+end
