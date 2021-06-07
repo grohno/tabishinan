af8edb662d23b0785b28b15cd51f592e614647b2
yuki ohno <jeanadler33@gmail.com>
2021-06-07 11:04:17 +0000

    追加: deviceコントローラーのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :guides
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   devise_for :users
 end
