b5ac40e05e04accae847f21c5b4d9f001306b8fb
yuki ohno <jeanadler33@gmail.com>
2021-06-07 10:25:20 +0000

    追加: ユーザーモデルのimageカラムにImageUploaderを紐付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :guides, dependent: :destroy
 
+  mount_uploader :image, ImageUploader
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
