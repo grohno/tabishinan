cf417b3a51622148114cf9cc2330f1867762dddb
yuki ohno <jeanadler33@gmail.com>
2021-06-19 12:32:26 +0000

    追加: ルーティングにgoogle認証された際のcallback URLを指定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
