e10d27bf37b3c10ea0f5122b3e6b27c0f2a588ae
yuki ohno <jeanadler33@gmail.com>
2021-06-19 12:57:15 +0000

    修正: ルーター設定とシードデータの修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :guides
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
     omniauth_callbacks: "users/omniauth_callbacks"
   }
   devise_scope :user do
