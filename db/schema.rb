418c4b9c836ca6d1e5fb3759a71cfaabbcb9d425
yuki ohno <jeanadler33@gmail.com>
2021-06-29 03:11:37 +0000

    追加: ガイドモデルに検索元のカラムを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_075146) do
+ActiveRecord::Schema.define(version: 2021_06_29_031020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_06_21_075146) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "search_field"
     t.index ["title"], name: "index_guides_on_title"
     t.index ["user_id"], name: "index_guides_on_user_id"
   end
